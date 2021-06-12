90838f53380fe1d0e2bf17b41c1081bc9ccea939
Laura Todica <laura.todica@gmail.com>
2021-06-12 07:04:24 +0300

    update api

--- scripts/forecast.js
+++ scripts/forecast.js
@@ -2,7 +2,7 @@ const key = 'axAvsJEZpQdNP4INHJ7TGZd9CKPuqPuc';
 
 const getWeather = async (weatherid) => {
 
-    const base = 'http://dataservice.accuweather.com/currentconditions/v1/';
+    const base = 'https://dataservice.accuweather.com/currentconditions/v1/';
     const query = `${weatherid}?apikey=${key}`;
 
     const response = await fetch (base + query);
@@ -14,7 +14,7 @@ const getWeather = async (weatherid) => {
 
 const getCity = async (city) => {
     
-    const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+    const base = 'https://dataservice.accuweather.com/locations/v1/cities/search';
     const query = `?apikey=${key}&q=${city}`;
 
     const response = await fetch(base + query);
